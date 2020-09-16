4e1b6974e2c5240dc00c210406fc8550b8a686b9
Love Jonsson <love.jonsson@gmail.com>
2020-09-16 22:20:56 +0200

    Added a skeleton of classes and functions

---
+++ BankApplicationLibrary/Customer.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankApplicationLibrary
+{
+    class Customer
+    {
+		public string FirstName { get; set; }
+		public string LastName { get; set; }
+		public string FullName
+		{
+			get
+			{
+				return $"{FirstName} {LastName}";
+			}
+		}
+
+		public long PersonalNumber { get; private set; }
+
+		public void CreateAccount()
+        {
+
+        }
+
+		public List<Account> Accounts { get; private set; } // NOT: inte implementerat klassen Account än
+
+		public Customer(long pNr, string firstName, string lastName)
+		{
+			// todo; Kolla först så att pNr inte redan är registrerat i banken genom att anropa en funktion för detta
+			PersonalNumber = pNr;
+			FirstName = firstName;
+			LastName = lastName;
+		}
+	}
+}
